d36a202546e1c77c14167e0aa7014a52650e17cc
birkoss <admin@birkoss.com>
2017-08-19 19:15:16 -0400

    Starting on ionic 3

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { ConfigProvider } from '../../providers/config/config';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl:NavController, public config:ConfigProvider) {
 
   }
 
